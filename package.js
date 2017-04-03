6466a3132f0c9bd54742b893a69a78753e67143b
Po Buchi <po@ox2.io>
2017-04-03 10:07:26 +0700

    #1 fix

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'pauli:linkedin-config-ui',
-  version: '1.0.0',
+  version: '1.1.0',
   // Brief, one-line summary of the package.
   summary: 'Blaze configuration templates for LinkedIn OAuth.',
   // URL to the Git repository containing the source code for this package.
