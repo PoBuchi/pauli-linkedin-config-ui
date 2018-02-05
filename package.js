e16ac5d5d835a7166b9610fb62d4688d8d892653
Po Buchi <po@ox2.io>
2018-02-05 14:07:33 +0700

    build

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   name: 'pauli:linkedin-config-ui',
-  version: '1.1.1',
+  version: '3.0.0',
   // Brief, one-line summary of the package.
-  summary: 'Blaze configuration templates for LinkedIn OAuth.',
+  summary: 'Blaze configuration templates for LinkedIn OAuth, use with Meteor 1.6.1 & up',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/PoBuchi/pauli-linkedin-config-ui',
   // By default, Meteor will default to using README.md for documentation.
@@ -11,7 +11,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.4.3.1');
+  api.versionsFrom('1.6.1');
   api.use('ecmascript');
   api.use('templating@1.2.13', 'client');
 
