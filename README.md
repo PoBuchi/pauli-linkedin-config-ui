fe9160c5c16013662934c5dc06231a427d530e42
Po Buchi <po@ox2.io>
2019-02-11 20:06:42 +0700

    linkedin-api-v2

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
+linkedin-config-ui
+============================
+
 This package contains Blaze configuration UI templates for LinkedIn OAuth.
+
+Install
+-----------
+```
+meteor add pauli:linkedin-config-ui
+```
+
+
+License
+-----------
+[MIT](https://github.com/PauliBuccini/meteor-accounts-linkedin/blob/master/LICENSE)
--- package.js
+++ package.js
@@ -1,22 +1,21 @@
 Package.describe({
   name: 'pauli:linkedin-config-ui',
-  version: '3.1.0',
-  // Brief, one-line summary of the package.
-  summary: 'Blaze configuration templates for LinkedIn OAuth, use with Meteor 1.6.1 & up',
-  // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/PoBuchi/pauli-linkedin-config-ui',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
-});
+  version: '4.0.0',
+  summary:
+    'Blaze configuration templates for LinkedIn OAuth, use with Meteor 1.6.1 & up',
+  git:
+    'https://github.com/PoBuchi/pauli-linkedin-config-ui',
+  documentation: 'README.md',
+})
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.6.1');
-  api.use('ecmascript');
-  api.use('templating@1.2.13', 'client');
+Package.onUse(api => {
+  api.versionsFrom('1.6.1')
+  api.use('ecmascript')
+  api.use('templating@1.2.13', 'client')
 
-  api.addFiles('linkedin-login-button.css', 'client');
+  api.addFiles('linkedin-login-button.css', 'client')
   api.addFiles(
     ['linkedin-configure.html', 'linkedin-configure.js'],
-    'client');
-});
+    'client',
+  )
+})
